25ae470faf05e8d2b4f692b24baa4d09cfc68c0e
Sebastian Felix Schwarz <sebastian.schwarz@publicispixelpark.de>
2017-03-30 22:18:45 +0200

    setup

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const webshot = require('webshot');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+webshot('google.com', 'google.png', function(err) {
+    // screenshot now saved to google.png
+});
+
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
